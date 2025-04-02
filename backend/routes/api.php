9d96875f828742ecf10ff993d63dc530ecd463d7
713koukou-naizaa <nzo.akt@gmail.com>
2025-04-02 12:26:12 +0200

    added api manual routes in comment

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,11 +7,26 @@ use Illuminate\Support\Facades\Route;
 
 /*
 apiResource creates these routes :
-GET /parties (index)
-POST /parties (store)
-GET /parties/{id} (show)
-PUT/PATCH /parties/{id} (update)
-DELETE /parties/{id} (destroy)
+// Parties routes
+Route::get('parties', [PartiesController::class, 'index']);
+Route::post('parties', [PartiesController::class, 'store']);
+Route::get('parties/{id}', [PartiesController::class, 'show']);
+Route::put('parties/{id}', [PartiesController::class, 'update']);
+Route::delete('parties/{id}', [PartiesController::class, 'destroy']);
+
+// Reservations routes
+Route::get('reservations', [ReservationsController::class, 'index']);
+Route::post('reservations', [ReservationsController::class, 'store']);
+Route::get('reservations/{id}', [ReservationsController::class, 'show']);
+Route::put('reservations/{id}', [ReservationsController::class, 'update']);
+Route::delete('reservations/{id}', [ReservationsController::class, 'destroy']);
+
+// Goodies routes
+Route::get('goodies', [GoodiesController::class, 'index']);
+Route::post('goodies', [GoodiesController::class, 'store']);
+Route::get('goodies/{id}', [GoodiesController::class, 'show']);
+Route::put('goodies/{id}', [GoodiesController::class, 'update']);
+Route::delete('goodies/{id}', [GoodiesController::class, 'destroy']);
 */
 Route::apiResource('parties', PartiesController::class);
 Route::apiResource('reservations', ReservationsController::class);
