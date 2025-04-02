efc0e3d687b864c2e0018495bd64eb752e2dced9
713koukou-naizaa <nzo.akt@gmail.com>
2025-04-02 14:25:54 +0200

    removed unused import

--- frontend/src/app/components/goodies/goodies.component.ts
+++ frontend/src/app/components/goodies/goodies.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { GoodiesService } from '../../services/goodies.service';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-goodies',
