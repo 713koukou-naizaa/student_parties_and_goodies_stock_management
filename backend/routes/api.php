0cb1e301728798ea8adc25a51547903458088a28
713koukou-naizaa <nzo.akt@gmail.com>
2025-04-02 11:23:20 +0200

    renamed json files names and refs for unicity, deleted redundant api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,21 +5,14 @@ use App\Http\Controllers\ReservationsController;
 use App\Http\Controllers\GoodiesController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('events', PartiesController::class);
+/*
+apiResource creates these routes :
+GET /parties (index)
+POST /parties (store)
+GET /parties/{id} (show)
+PUT/PATCH /parties/{id} (update)
+DELETE /parties/{id} (destroy)
+*/
+Route::apiResource('parties', PartiesController::class);
 Route::apiResource('reservations', ReservationsController::class);
 Route::apiResource('goodies', GoodiesController::class);
-
-Route::get('/events', function () {
-    $eventsPath = storage_path('json/events.json');
-    return response()->json(json_decode(file_get_contents($eventsPath), true));
-});
-
-Route::get('/reservations', function () {
-    $reservationsPath = storage_path('json/reservations.json');
-    return response()->json(json_decode(file_get_contents($reservationsPath), true));
-});
-
-Route::get('/goodies', function () {
-    $goodiesPath = storage_path('json/goodies.json');
-    return response()->json(json_decode(file_get_contents($goodiesPath), true));
-});
