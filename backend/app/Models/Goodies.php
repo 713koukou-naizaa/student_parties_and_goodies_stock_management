53cd287dcf0dbd81fd8a281cf3e767cc540c0f20
713koukou-naizaa <nzo.akt@gmail.com>
2025-04-02 11:34:08 +0200

    deleted migrations as useless, added save and all functions to newly created models (adapted to json files)

---
+++ backend/app/Models/Goodies.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Goodies extends Model
+{
+    protected static $filePath = 'json/goodies.json';
+
+    public static function getFilePath(): string { return self::$filePath; }
+
+    public static function all(): array { return storage_path(json_decode(file_get_contents(self::$filePath)), true); }
+
+    public static function save(array $pData): void { file_put_contents(storage_path(self::$filePath), json_encode($pData, JSON_PRETTY_PRINT)); }
+}
